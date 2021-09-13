6d7491a5fca33e9ce289615a4dc6ada6d613bea6
Jillyp <Jillyp@172.16.139.117>
2021-09-13 11:24:25 -0600

    Added a method.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,18 @@
 
 public class HelloWorld {
 	
-
+	public String PrintStuff() {
+		return ("Merging branches");
+		
+	}
 	public static void main(String[] args) {
+		HelloWorld hello = new HelloWorld();
 		
 		System.out.println("Hello World");
 		System.out.println("Hello Jill");
+		hello.PrintStuff();
+		
 	}
+		
 
 }
