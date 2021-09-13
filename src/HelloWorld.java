a57872c4eade99a991cbe70cde34f84831ff3deb
Richard Wang <richardbdgbcs@gmail.com>
2021-09-13 11:30:33 -0600

    Add printStuff() in main class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,14 @@ public class HelloWorld {
 	
 
 	public static void main(String[] args) {
-		
+		HelloWorld hw = new HelloWorld();
 		System.out.println("Hello World");
 		System.out.println("Hello Jill");
+		hw.printStuff();
+	}
+	
+	public String printStuff() {
+		return "Richard's Branch";
 	}
 
 }
